173f12438c6646d950d6334d861e5a59af391994
jonh-etc <jon.herman20@gmail.com>
2025-08-11 13:31:21 -0600

    use native python apis instead of subprocess

--- vuln-1.py
+++ vuln-1.py
@@ -8,14 +8,14 @@ app = flask.Flask(__name__)
 @app.route("/route_param/<route_param>")
 def route_param(route_param):
     allowed_commands = {
-        "uptime": ["uptime"],
-        "ls": ["ls", "-l"],
+        "ls": os.listdir,
+        "pwd": os.getcwd,
     }
 
     if route_param not in allowed_commands:
         return "nice try!"
 
-    subprocess.call(allowed_commands.get(route_param))
+    allowed_commands.get(route_param)()
 
     return "slightly better"
 
